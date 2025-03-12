f211b43213191bd34f08f9649662b7931adb2960
nabillazian <242310040@student.ibik.ac.id>
2025-03-12 23:27:38 +0700

    Add files via upload

---
+++ PTM 2 - Latihan IF.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	
+	cout << "Masukkan sebuah angka: ";
+	cin >> a;
+	
+	if ( a % 2 == 0)
+		cout << a << " adalah bilangan genap.";
+	else 
+		cout << a << " adalah bilangan ganjil.";
+}
